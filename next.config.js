44f8ed8ffe3f8181bf78afb33d4d3036a8342b63
OleksandrB93 <barabashsan4ik@gmail.com>
2023-09-22 23:47:30 +0300

    test publication

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {}
 
-module.exports = {
-  basePath: "/portfolio", // Шлях, який ви хочете додати до URL
-};
+module.exports = nextConfig
